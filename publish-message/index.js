dc020d8d27b29393d3c7891efbf43ae2bbce7e46
mab777 <martin.a.balmaceda@gmail.com>
2020-11-17 14:01:18 -0300

    subida

--- publish-message/index.js
+++ publish-message/index.js
@@ -5,13 +5,32 @@ const sqs = new AWS.SQS();
 
 exports.handler = async (event) => {
     try {
-        // All messages on SQS *MUST* be a String
-        await sqs.sendMessage({
-            MessageBody: JSON.stringify({
-                name: event.name || 'Prueba'
-            }),
-            QueueUrl: 'https://sqs.us-east-1.amazonaws.com/437051718508/Pepe-queue'
-        }).promise();
+
+        if (event.name !== typeof undefined && event.name !== null) {
+            const expReg = new RegExp(/[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-]/g);
+            const name = event.name;
+
+            if (!expReg.test(name)) {
+                // All messages on SQS *MUST* be a String
+                await sqs.sendMessage({
+                    MessageBody: JSON.stringify({
+                        name: name
+                    }),
+                    QueueUrl: 'https://sqs.us-east-2.amazonaws.com/464975321668/test'
+                }).promise();
+            } else {
+                console.log("ENTRE EN DE CHARS")
+                return {
+                    statusCode: 200,
+                    body: "El parámetro name, no debe contener carácteres especiales"
+                }
+            }
+        } else {
+            return {
+                statusCode: 406,
+                body: "Es necesario que envie el parámetro"
+            }
+        }
     }
     catch (err) {
         console.log(err);
@@ -22,5 +41,5 @@ exports.handler = async (event) => {
         };
     }
 
-    return { statusCode: 200 };
+    return { statusCode: 200, body: "mensaje enviado" };
 };
